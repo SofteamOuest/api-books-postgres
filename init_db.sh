6c7c46c6b896ce1880d262c66cb078b1782e95e6
Mehdi Elkouhen <mehdi.elkouhen@softeam.fr>
2017-12-03 20:08:48 +0100

    wip

--- init_db.sh
+++ init_db.sh
@@ -4,6 +4,4 @@ set -e
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     CREATE ROLE apibook WITH LOGIN PASSWORD 'apibook';
     CREATE DATABASE apibook WITH TEMPLATE = template0 OWNER = apibook ;
-    \\connect apibook;
-    CREATE SCHEMA apibook AUTHORIZATION apibook;
 EOSQL
